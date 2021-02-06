9c4f5d3cd79e850c5ac210c83ddf57227d21af9b
leopoidev <Utilisateur@DESKTOP-F3D72VM>
2021-02-06 00:25:24 -0500

    wdawd

---
+++ src/main/java/net/mcreator/jonemodv/item/MotorvItem.java
@@ -0,0 +1,47 @@
+
+package net.mcreator.jonemodv.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.jonemodv.JoneModVModElements;
+
+@JoneModVModElements.ModElement.Tag
+public class MotorvItem extends JoneModVModElements.ModElement {
+	@ObjectHolder("jone_mod_v:motorv")
+	public static final Item block = null;
+	public MotorvItem(JoneModVModElements instance) {
+		super(instance, 43);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON));
+			setRegistryName("motorv");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
